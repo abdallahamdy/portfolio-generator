f365e782f68fdc14482e54239775b5d9d436c75b
AHamdy <abdulla.mh@hotmail.com>
2022-08-22 00:53:39 -0400

    integrated input validation functionality

--- app.js
+++ app.js
@@ -21,7 +21,15 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'github',
-        message: 'Enter your GitHub Username'
+        message: 'Enter your GitHub Username (Required)',
+        validate: usernameInput => {
+            if (usernameInput) {
+              return true;
+            } else {
+              console.log('Please enter your username!');
+              return false;
+            }
+        }
       },
       {
         type: 'input',
@@ -47,12 +55,28 @@ const promptProject = portfolioData => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your project?'
+        message: 'What is your name? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your name!');
+            return false;
+          }
+        }
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)'
+        message: 'Provide a description of the project (Required)',
+        validate: descriptionInput => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log('Please enter a description!');
+            return false;
+          }
+        }
       },
       {
         type: 'checkbox',
@@ -63,7 +87,15 @@ const promptProject = portfolioData => {
       {
         type: 'input',
         name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)'
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: linkInput => {
+            if (linkInput) {
+              return true;
+            } else {
+              console.log('Please enter a link!');
+              return false;
+            }
+          }
       },
       {
         type: 'confirm',
